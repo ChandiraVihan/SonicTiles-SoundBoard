daa024f4838449e0f2fe43a934c59df24ed651c1
ChandiraVihan <chandiravihan@gmail.com>
2025-12-17 23:32:57 +0530

    challenge-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import { useState } from 'react'
-import './App.css'
+import pads from "./pads"
 
-function App() {
-  
-
-  return (
-    <>
-      <h1>Sonic Tiles</h1>
-    </>
-  )
+export default function App() {
+    /**
+     * Challenge part 1:
+     * 1. Initialize state with the default value of the
+     *    array pulled in from pads.js
+     * 2. Map over that state array and display each one
+     *    as a <button> (CSS is already written for you)
+     *    (Don't worry about using the "on" or "color" 
+     *    properties yet)
+     */
+    return (
+        <main>
+            <div className="pad-container">
+                {/* <button>s go here */}
+            </div>
+        </main>
+    )
 }
-
-export default App
