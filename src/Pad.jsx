fa05004c5f66578f460df08cfae174578c69ad1b
ChandiraVihan <chandiravihan@gmail.com>
2025-12-18 15:45:26 +0530

    isOn=on(in props)

--- src/Pad.jsx
+++ src/Pad.jsx
@@ -1,9 +1,18 @@
+import { useState } from "react"
 import "./App.css"
 
 function Pad(props) {
+
+    const [isOn, setIsOn] = useState(props.on);
+
+    function toggle() {
+        setIsOn(prevIsOn => !prevIsOn);
+    }
+
     return (
  <button style={{ backgroundColor: props.color }}
- className ={props.on ? "on" : ""}
+    className ={isOn ? "on" : ""} 
+    onClick={toggle}
  ></button>
     )
 }
