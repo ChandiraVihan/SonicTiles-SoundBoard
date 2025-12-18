4ec14f7425afbf1a96252a28fc73be89269e9510
ChandiraVihan <chandiravihan@gmail.com>
2025-12-18 12:53:00 +0530

    hard-codes0colors-for-buttons

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,23 @@ import pads from "./pads"
 import "./App.css"
 
 function App() {
-    /**
-     * Challenge part 1:
-     * 1. Initialize state with the default value of the
-     *    array pulled in from pads.js
-     * 2. Map over that state array and display each one
-     *    as a <button> (CSS is already written for you)
-     *    (Don't worry about using the "on" or "color" 
-     *    properties yet)
-     */
 
     const [padArray, setPadArray] = useState(pads)
+
+
+    const style = {
+        backgroundColor: "lightblue"}
+
+    const buttonElements = padArray.map (pad => (
+        <button style = {style}                     //inline styling example
+            key={pad.id} >  </button>))
+
     return (
         <>
           <h1> SonicTiles</h1>
           <main>
             <div className="pad-container">
-                {padArray.map(pad => (
-                    <button key={pad.id}></button>
-                ))}
+                {buttonElements}
             </div>
         </main>
         </>
